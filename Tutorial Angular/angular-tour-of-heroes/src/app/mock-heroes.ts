8e309890f356f664b2f4a8b8c1166908359c5a5c
AndreiCroitoriu-cpu <andreicroitoriu@icloud.com>
2022-10-25 11:45:36 +0300

    First commit

---
+++ Tutorial Angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 12, name: 'Mary' },
+  {id: 13, name: 'Rohan' },
+  {id: 14, name: 'Lillie' },
+  {id: 15, name: 'Henri' },
+  {id: 16, name: 'Dawson' },
+  {id: 17, name: 'Lister' },
+  {id: 18, name: 'Sakina' },
+  {id: 19, name: 'Ivor' },
+  {id: 20, name: 'Ion' }
+];
